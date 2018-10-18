2de0f82521d52010cb42edf1042e33a8a713f7cc
anfgc <moena1989@gmail.com>
2018-10-18 11:58:08 -0500

    Se agrega binding a los campos y se organiza la estructura general para el serial.
    Se ha agregado la libreria para realizar los hash.

--- src/app/nuevo-ingreso-reloj/nuevo-ingreso-reloj.component.ts
+++ src/app/nuevo-ingreso-reloj/nuevo-ingreso-reloj.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {HasherService} from '../services/hasher.service';
+import {DbService} from '../services/db.service';
 
 @Component({
   selector: 'app-nuevo-ingreso-reloj',
@@ -6,11 +8,88 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./nuevo-ingreso-reloj.component.css']
 })
 export class NuevoIngresoRelojComponent implements OnInit {
+  /*/
+  variables de seleccion
+   */
 
-  constructor() {
+  modelo: any[] = [];
+  coleccion: any[] = [];
+  material: any[] = [];
+  maquinaria: any[] = [];
+  color_maq: any[] = [];
+  mat_pulso: any[] = [];
+  madera: any[] = [];
+  color_pulso: any[] = [];
+  serial_raw: string;
+
+  manos: any[] = [];
+  lote: any[] = [];
+  piezas: any[] = [];
+
+  constructor(private hasher: HasherService, public db: DbService) {
   }
 
   ngOnInit() {
   }
 
+  iniciarNuevoRegistro() {
+
+    // se confirma que todos los datos fueron ingresados
+    if (this.comprobarCampos()) {
+      this.hasher.createHashReloj();
+
+
+      this.serial_raw =
+        this.manos.salt +
+        this.lote.salt +
+        this.piezas.salt + '-' +
+        this.modelo.salt +
+        this.coleccion.salt +
+        this.material.salt +
+        this.maquinaria.salt +
+        this.color_maq.salt +
+        this.mat_pulso.salt +
+        this.madera.salt;
+
+      console.log(this.serial_raw);
+
+    } else {
+      console.log('Revisa todos los campos para poder continuar!');
+    }
+  }
+
+  comprobarCampos(): boolean {
+    console.log('Se comprueban los campos');
+    if (this.modelo === null) {
+      return false;
+    }
+
+    if (this.coleccion === null) {
+      return false;
+    }
+
+    if (this.material === null) {
+      return false;
+    }
+
+    if (this.maquinaria === null) {
+      return false;
+    }
+
+    if (this.color_maq === null) {
+      return false;
+    }
+
+    if (this.mat_pulso === null) {
+      return false;
+    }
+
+    if (this.madera === null) {
+      return false;
+    }
+    if (this.color_pulso === null) {
+      return false;
+    }
+    return true;
+  }
 }
